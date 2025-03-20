13c6a51ac7a662f12cb7cfae6a1a52496b21fc30
Robert-Ioan Constantinescu <robertioan.constantinescu@keyight.com>
2025-03-20 19:00:51 +0000

    initial commit
    Signed-off-by: Robert-Ioan Constantinescu <robertioan.constantinescu@keyight.com>

---
+++ install-debian.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Upgrade system & Add some useful packages
+sudo apt-get update -y && sudo apt-get upgrade -y && \
+sudo apt-get install -y \
+    build-essential \
+    tcpreplay \
+    lsd \
+    ripgrep \
+    python3.12-venv \
+    python3.12-dev \
+    libreadline-dev \
+    npm \
+    tmux \
+    zsh \
+    zoxide
+
+# Fast Python package manager (uv)
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# Install neovim 0.10.4
+command -v nvim > /dev/null || ( \
+    wget https://github.com/neovim/neovim-releases/releases/download/v0.10.4/nvim-linux-x86_64.deb && \
+    sudo apt install -y ./nvim-linux-x86_64.deb && \
+    rm ./nvim-linux-x86_64.deb \
+)
+
+sudo usermod -s $(which zsh) "$USER"
+
+NVIM_CONFIG_PATH="$HOME"/.config/nvim
+TMUX_CONFIG_PATH="$HOME"/.config/tmux
+ZSH_CONFIG_PATH="$HOME"/.config/zsh
+
+[ ! -d $NVIM_CONFIG_PATH ] && \
+    git clone https://github.com/RCX777/neovim-config $NVIM_CONFIG_PATH
+
+[ ! -d $ZSH_CONFIG_PATH ] && \
+    git clone https://github.com/RCX777/zsh-config && \
+    cd zsh-config && \
+    . ./install.sh && \
+    cd .. && \
+    rm -rf zsh-config
+
+[ ! -d $TMUX_CONFIG_PATH ] && \
+    git clone https://github.com/RCX777/tmux-config && \
+    cd tmux-config && \
+    . ./install.sh && \
+    cd .. && \
+    rm -rf zsh-config
+
